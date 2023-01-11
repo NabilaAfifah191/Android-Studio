c267e1ae4e5735768264dde0ec1d5e25abb4c89a
nabilaafifah191 <divarah.loviane@gmail.com>
2023-01-11 17:34:45 +0800

    UTS

---
+++ Pertemuan 1/CodingChallenge1_1/app/src/main/java/id/ac/poliban/va/nabila/e020320021/codingchallenge1_1/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.poliban.va.nabila.e020320021.codingchallenge1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.poliban.va.dwi.e020320002.codingchallenge1_1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Pertemuan 8/Homework4_4/app/src/main/java/id/ac/poliban/mi/nabila/e020320021/homework4_4/FourthActivity.java
@@ -0,0 +1,6 @@
+package id.ac.poliban.mi.nabila.e020320021.homework4_4;
+
+import android.app.Activity;
+
+public class FourthActivity extends Activity {
+}
